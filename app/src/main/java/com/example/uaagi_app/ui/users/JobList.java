be014489acde90f44f2bbb03dc9cd15152a0115e
天使 <angelemiejane.bien@gmail.com>
2026-02-11 16:01:31 +0800

    complete pre emp form, still no preview

--- app/src/main/java/com/example/uaagi_app/ui/users/JobList.java
+++ app/src/main/java/com/example/uaagi_app/ui/users/JobList.java
@@ -2,7 +2,7 @@ package com.example.uaagi_app.ui.users;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +10,27 @@ import androidx.cardview.widget.CardView;
 
 import com.example.uaagi_app.R;
 
-public class HomePage extends AppCompatActivity {
-    private static final String TAG = "MainActivityLifecycle";
+public class JobList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "MainAct onCreate()");
         EdgeToEdge.enable(this);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.job_list);
+
 
         // Initialize the CardView
-        CardView firstJobCard = findViewById(R.id.firstjob);
+        CardView fotonjobCard = findViewById(R.id.fotonjob);
 
         // Set click listener
-        firstJobCard.setOnClickListener(v -> {
+        fotonjobCard.setOnClickListener(v -> {
             // Create intent to navigate to job details page
-            Intent intent = new Intent(HomePage.this, JobList.class);
+            Intent intent = new Intent(JobList.this, JobDesc.class);
 
 
             // Start the activity
             startActivity(intent);
         });
+
     }
 }
