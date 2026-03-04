586ca434d5ea4fe2214181636275cc433b774c05
Kenji <kenjichua590@gmail.com>
2026-03-04 15:12:12 +0800

    feats: apply with resme now working, with proper feedback

--- app/src/main/java/com/example/uaagi_app/network/api/ApplicationApi.java
+++ app/src/main/java/com/example/uaagi_app/network/api/ApplicationApi.java
@@ -2,10 +2,14 @@ package com.example.uaagi_app.network.api;
 
 import com.example.uaagi_app.network.dto.ApiResponse;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApplicationApi {
 
@@ -15,5 +19,14 @@ public interface ApplicationApi {
             @Field("user_id") int userId,
             @Field("job_id") int jobId,
             @Field("apply_method") String applyMethod
+    );
+
+    @Multipart
+    @POST("index.php/application-send")
+    Call<ApiResponse> submitResumeApplication(
+            @Part("user_id") RequestBody userId,
+            @Part("job_id") RequestBody jobId,
+            @Part("apply_method") RequestBody applyMethod,
+            @Part MultipartBody.Part file
     );
 }
